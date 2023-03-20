2cbe3c9ee55700e933b0eb1e73a39f1f7de80683
Ana150 <79976673+Ana150@users.noreply.github.com>
2023-03-20 10:24:58 -0300

    20/03

--- aula6/exercicios.py
+++ aula6/exercicios.py
@@ -1,31 +1,22 @@
-''' #Dadas duas notas, calcular a média das duas notas e verificar se o aluno foi aprovado (media 6)
 n1 = float(input("Digite a nota 1: "))
-n2 = float(input("Digite a nota 2: "))
-media = (n1 + n2) / 2
-
-if media > 6:
-    print(f"Aprovado com media {media}")
-    else:    print(f"Reprovado com media {media}")
-
-V2.0 - Considere o enunciado da versão anterior. Contudo, consistir os dados
-para que a medianáo seja calculada de forma errada.
-TESTES
-Nota 1: 4   Nota 2: 5       SAÍDA: Reprovado com media 4.5
-Nota 1: 7   Nota 2: 15      SAÍDA: A segunda nota é inválida
-Nota 1: -3                  SAÍDA: A primeira nota é inválida
-'''
-
- n1 = float(input("Digite a nota 1: "))
- media = (n1 + n2) / 2
-
- if n1 >0 and n1<10:
-     n2 = float(input("Digite a nota 2: "))
-     if n2 >0 and n2<10:
-         if media > 6:
-             print(f"Aprovado com media {media}")
-         else:
-             print(f"Reprovado com media {media}")
- else:
-     prit(f"{n1} é uma nota inválida. Tente novamente!")
- media = (n1 + n2) / 2
+if n1 >= 0 and n1 <= 10:
+    n2 = float(input("Digite a nota 2: "))
+    if n2 >0 and n2<10:
+        media = (n1 + n2) / 2
+        if media >= 6:
+            print(f"APROVADO -> Média {int(media)}")
+        elif media >4 and media <= 5.9:
+            exame = float(input("Digite a nota do exame: "))
+            if exame >= 0 and exame <= 10:
+                mediaEx = (media + exame) / 2
+                if mediaEx >=6:
+                    print(f"APROVADO -> Nota pré exame: {media} | Nota pós exame: {mediaEx}")
+                else:
+                    print(f"REPROVADO -> Nota pré exame: {media} | Nota pós exame: {mediaEx}")
+        else:
+            print(f"REPROVADO -> Média {int(media)}")
+    else:
+        print(f"{int(n2)} é uma nota inválida. Tente novamente!")
+else:
+    print(f"{int(n1)} é uma nota inválida. Tente novamente!")
 
