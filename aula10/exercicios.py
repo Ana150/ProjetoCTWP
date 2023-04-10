add34f4b7964c4ccbc2c6ef47a94d445952380e8
Ana150 <79976673+Ana150@users.noreply.github.com>
2023-04-10 09:56:23 -0300

    10/04

---
+++ aula10/exercicios.py
@@ -0,0 +1,5 @@
+n1 = int(input("Digite o valor inicial: "))
+
+while n1 <= (n1*10):
+    x = x*n1
+    
---
+++ aula10/lacos.py
@@ -0,0 +1,53 @@
+#LAÇO PRÉ-DETERMINADO
+
+#Ler valor inicial
+'''n1 = int(input("Digite o valor inicial: "))
+
+#Ler valor final
+n2 = int(input("Digite o valor final: "))
+'''
+'''#Inicio do laço
+while n1 <= n2:
+    #exibir os números
+    print(n1)
+
+    #somar 1 ao próximo
+    n1 = n1 + 2
+else:#é executado quando não há interferência no laço
+    print("Tudo certo")
+#Final do laço'''
+
+#EXERCICIO LAÇO FECHADO
+'''if n1 > n2:
+    print("O segundo número deve ser menor que o primeiro")
+else:
+    n1 = n1 + 1
+    n2 = n2 - 1
+    while n1 <= n2:
+        # exibir os números
+        print(n1)
+
+        # somar 1 ao próximo
+        n1 = n1 + 1'''
+
+#While True
+'''while True:
+    print(n1)
+    n1 = n1 + 2
+    print("Executou o bloco")
+    if n1 > n2:
+        break'''
+
+opcao = "S"
+while opcao == "S":
+    n1 = int(input("Digite o valor inicial: "))
+    n2 = int(input("Digite o valor final: "))
+
+    while n1 <= n2:
+        print(n1)
+        n1 = n1 + 2
+
+    opcao = input("Deseja continuar (S/N)?: ").upper()
+    while opcao.upper() != 'S' and opcao.upper() != 'N':
+        print("Erro! Digite S ou N")
+        opcao = input("Deseja continuar (S/N)?: ")
